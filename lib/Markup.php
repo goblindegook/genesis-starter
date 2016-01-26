9837045cd94c1c5aff1a4889eae0683c633cef49
Luis Rodrigues <goblindegook@users.noreply.github.com>
2016-01-26 00:34:42 +0000

    head attributes

--- lib/Markup.php
+++ lib/Markup.php
@@ -17,13 +17,13 @@ class Markup {
  	*/
 	public function doctype() {
 		?><!DOCTYPE html>
-		<!--[if lt IE 7 ]><html class="ie6 no-js" <?php language_attributes( 'html' ); ?>><![endif]-->
-		<!--[if IE 7 ]><html class="ie7 no-js" <?php language_attributes( 'html' ); ?>><![endif]-->
-		<!--[if IE 8 ]><html class="ie8 no-js" <?php language_attributes( 'html' ); ?>><![endif]-->
-		<!--[if IE 9 ]><html class="ie9 no-js" <?php language_attributes( 'html' ); ?>><![endif]-->
-		<!--[if (gt IE 9)|!(IE)]><!--><html class="no-js" <?php language_attributes( 'html' ); ?>><!--<![endif]-->
-		<head>
-			<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<!--[if lt IE 7 ]><html class="ie6 no-js" <?php \language_attributes( 'html' ); ?>><![endif]-->
+		<!--[if IE 7 ]><html class="ie7 no-js" <?php \language_attributes( 'html' ); ?>><![endif]-->
+		<!--[if IE 8 ]><html class="ie8 no-js" <?php \language_attributes( 'html' ); ?>><![endif]-->
+		<!--[if IE 9 ]><html class="ie9 no-js" <?php \language_attributes( 'html' ); ?>><![endif]-->
+		<!--[if (gt IE 9)|!(IE)]><!--><html class="no-js" <?php \language_attributes( 'html' ); ?>><!--<![endif]-->
+		<head <?php echo \genesis_attr( 'head' ); ?>>
+			<meta charset="<?php \bloginfo( 'charset' ); ?>" />
 		<?php
 	}
 
